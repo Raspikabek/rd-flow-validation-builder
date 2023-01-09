5c06cde61cbfe34da7a2aaa060bd159e089b9675
Eduardo Asenjo <easenjo@raspisdojo.com>
2023-01-09 21:17:57 +0000

    feat: add font-weith and index attribute to each row

--- force-app/main/default/lwc/rdExpressionListItem/rdExpressionListItem.js
+++ force-app/main/default/lwc/rdExpressionListItem/rdExpressionListItem.js
@@ -4,6 +4,13 @@ export default class RdExpressionListItem extends LightningElement {
   @api
   expression;
 
+  @api
+  rowIndex;
+
+  get rowNumber() {
+    return this.rowIndex + 1;
+  }
+
   handleDeleteButton() {
     this.dispatchEvent(
       new CustomEvent("delete", { detail: this.expression.name })
